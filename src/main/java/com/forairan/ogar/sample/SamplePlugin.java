9b9be3073919f65547594325a52696a13515e92d
Devin Ryan <devin@forairan.com>
2015-08-10 15:02:48 -0500

    Initial commit

---
+++ src/main/java/com/forairan/ogar/sample/SamplePlugin.java
@@ -0,0 +1,23 @@
+package com.forairan.ogar.sample;
+
+import com.ogarproject.ogar.api.plugin.Plugin;
+import com.ogarproject.ogar.api.plugin.PluginInfo;
+
+@PluginInfo(
+        name = "Sample Plugin",
+        version = "1.0",
+        author = "Forairan",
+        description = "A sample plugin for Ogar 2."
+)
+public class SamplePlugin extends Plugin {
+
+    @Override
+    public void onDisable() {
+        getLogger().info("SamplePlugin#onDisable called!");
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("SamplePlugin#onEnable called!");
+    }
+}
