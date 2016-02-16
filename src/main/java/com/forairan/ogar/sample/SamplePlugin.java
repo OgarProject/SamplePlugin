3255a4ba822df7aac6b0e1958b95ac1382a1fcb0
Devin Ryan <devin@forairan.com>
2016-02-15 21:02:59 -0600

    Add some functionality to test the Scheduler API

--- src/main/java/com/forairan/ogar/sample/SamplePlugin.java
+++ src/main/java/com/forairan/ogar/sample/SamplePlugin.java
@@ -19,7 +19,23 @@ public class SamplePlugin extends Plugin {
     @Override
     public void onEnable() {
         getLogger().info("SamplePlugin#onEnable called!");
+
+        // Register a test listener
         getPluginManager().registerEvents(this, new SampleListener(this));
+
+        // Register a few test tasks
+        getServer().getScheduler().runTaskLater(this, () -> System.out.println("runTaskLater - 20 tick delay, sync"), 20L);
+        getServer().getScheduler().runAsyncTaskLater(this, () -> System.out.println("runAsyncTaskLater - 40 tick delay, async"), 40L);
+        getServer().getScheduler().runTaskTimer(this, () -> System.out.println("runTaskTimer - 100 tick delay, 10 tick interval, sync"), 100L, 10L);
+        getServer().getScheduler().runAsyncTaskTimer(this, () -> System.out.println("runAsyncTaskTimer - 100 tick delay, 10 tick interval, async"), 100L, 10L);
+        
+        // How about a misbehaving task?
+        getServer().getScheduler().runTaskLater(this, () -> {
+            // ;)
+            System.out.println("Misbehaving task test - throwing NPE now!");
+            throw new NullPointerException();
+        }, 1L);
+
         getLogger().info("SamplePlugin: listener registered.");
     }
 }
