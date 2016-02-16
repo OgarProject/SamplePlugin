77adfa314aa7a463c6dadaca58fbd86774b7f7a1
Devin Ryan <devin@forairan.com>
2016-02-15 21:12:34 -0600

    Clean up, and add a test for task cancellation

--- src/main/java/com/forairan/ogar/sample/SamplePlugin.java
+++ src/main/java/com/forairan/ogar/sample/SamplePlugin.java
@@ -23,6 +23,13 @@ public class SamplePlugin extends Plugin {
         // Register a test listener
         getPluginManager().registerEvents(this, new SampleListener(this));
 
+        // Run some scheduler tests
+        testScheduler();
+
+        getLogger().info("SamplePlugin: listener registered.");
+    }
+    
+    private void testScheduler() {
         // Register a few test tasks
         getServer().getScheduler().runTaskLater(this, () -> System.out.println("runTaskLater - 20 tick delay, sync"), 20L);
         getServer().getScheduler().runAsyncTaskLater(this, () -> System.out.println("runAsyncTaskLater - 40 tick delay, async"), 40L);
@@ -35,7 +42,10 @@ public class SamplePlugin extends Plugin {
             System.out.println("Misbehaving task test - throwing NPE now!");
             throw new NullPointerException();
         }, 1L);
-
-        getLogger().info("SamplePlugin: listener registered.");
+        
+        // Or cancelling a task?
+        int id = getServer().getScheduler().runTaskLater(this, () -> System.out.println("You should never see this message"), 20L);
+        // ...using another task?
+        getServer().getScheduler().runTaskLater(this, () -> getServer().getScheduler().cancelTask(id), 10L);
     }
 }
